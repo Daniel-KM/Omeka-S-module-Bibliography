c78f7b3ec9529a5a7a12c0672c7e53f70e0a3433
Daniel Berthereau <Daniel.github@Berthereau.net>
2019-11-25 00:00:00 +0100

    Fixed use of style and locale in view helper.

--- src/View/Helper/Citation.php
+++ src/View/Helper/Citation.php
@@ -13,7 +13,7 @@ class Citation extends AbstractHelper
      * @see https://docs.citationstyles.org/en/1.0.1/index.html
      *
      * @param AbstractResourceEntityRepresentation $resource
-     * @param array $options Managed options are: "append_site",
+     * @param array $options Managed options are: "style", "locale", "append_site",
      * "append_access_date", "tag", and "template".
      * The default options are used for Omeka resources. You may unset options
      * to append the site and the access date for real bibliographic resources.
@@ -26,6 +26,8 @@ class Citation extends AbstractHelper
 
         // Set default options.
         $options += [
+            'style' => null,
+            'locale' => null,
             'append_site' => true,
             'append_access_date' => true,
             'tag' => 'p',
@@ -34,8 +36,12 @@ class Citation extends AbstractHelper
         $options['csl'] = $this->convertToCsl($resource);
 
         $siteSetting = $view->plugin('siteSetting');
-        $options['style'] = $siteSetting('bibliography_csl_style') ?: 'chicago-fullnote-bibliography';
-        $options['locale'] = $siteSetting('bibliography_csl_locale') ?: str_replace('_', '-', $siteSetting('locale'));
+        if (is_null($options['style'])) {
+            $options['style'] = $siteSetting('bibliography_csl_style') ?: 'chicago-fullnote-bibliography';
+        }
+        if (is_null($options['locale'])) {
+            $options['locale'] = $siteSetting('bibliography_csl_locale') ?: str_replace('_', '-', $siteSetting('locale'));
+        }
 
         $template = empty($options['template']) ? 'common/citation' : $options['template'];
         unset($options['template']);
